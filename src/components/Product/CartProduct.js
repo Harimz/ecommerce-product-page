9a8460ab299f12c14d0b3e586517df932a3bfdec
harimz <harimzermeno.z@gmail.com>
2022-01-20 16:28:25 -0800

    Nearly completed landing page, add tablet support next

--- src/components/Product/CartProduct.js
+++ src/components/Product/CartProduct.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import {
+  AddInput,
+  Button,
+  ColoredBox,
+  CrossedOutText,
+  DetailsContainer,
+  Flex,
+  Header,
+  InputButton,
+  SaleHeader,
+  SubHeader,
+  Text,
+} from "./Styles/Index";
 
 const CartProduct = () => {
-  return <div>CART ME</div>;
+  const [itemAmount, setItemAmount] = useState(0);
+
+  return (
+    <DetailsContainer>
+      <SubHeader>SNEAKER COMPANY</SubHeader>
+      <Header size="3" mb="2">
+        Fall Limited Edition Sneakers
+      </Header>
+      <Text>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they'll withstand everything the
+        weather can offer
+      </Text>
+      <Flex>
+        <Header size="2">$125.00</Header>
+        <ColoredBox>
+          <SaleHeader>50%</SaleHeader>
+        </ColoredBox>
+      </Flex>
+      <CrossedOutText>$250</CrossedOutText>
+      <Flex mobileDir="column">
+        <AddInput>
+          <InputButton
+            onClick={() => {
+              if (itemAmount > 0) {
+                setItemAmount((state) => state - 1);
+              }
+            }}
+          >
+            -
+          </InputButton>
+          <Header size="1.5">{itemAmount}</Header>
+          <InputButton
+            onClick={() => {
+              if (itemAmount < 5) {
+                setItemAmount((state) => state + 1);
+              }
+            }}
+          >
+            +
+          </InputButton>
+        </AddInput>
+
+        <Button>
+          <FaShoppingCart />
+          <p>Add to cart</p>
+        </Button>
+      </Flex>
+    </DetailsContainer>
+  );
 };
 
 export default CartProduct;
--- src/components/Product/Styles/Index.js
+++ src/components/Product/Styles/Index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components/macro";
 
 export const Container = styled.div`
-  max-width: 60rem;
+  max-width: 75rem;
   width: 100%;
   margin: 0 auto;
   display: flex;
@@ -14,7 +14,7 @@ export const Container = styled.div`
 `;
 
 export const ProductGallery = styled.div`
-  width: 50%;
+  width: 40%;
 
   @media (max-width: 650px) {
     width: 100%;
@@ -43,5 +43,118 @@ export const SelectView = styled.div`
 
   @media (max-width: 650px) {
     display: none;
+  }
+`;
+
+export const DetailsContainer = styled.div`
+  width: 60%;
+  padding: 6rem;
+
+  @media (max-width: 650px) {
+    padding: 1rem;
+    width: 100%;
+  }
+`;
+
+export const SubHeader = styled.h3`
+  color: var(--primary);
+  font-weight: bold;
+  margin-bottom: 1rem;
+`;
+
+export const Header = styled.h1`
+  font-size: ${({ size }) => size + "rem"};
+  margin-bottom: ${({ mb }) => mb + "rem"};
+
+  @media (max-width: 650px) {
+    font-size: 2rem;
+  }
+`;
+
+export const Text = styled.p`
+  color: var(--dark-gray-blue);
+  font-size: 1.25rem;
+  line-height: 30px;
+  margin-bottom: 2rem;
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  grid-gap: 2rem;
+
+  @media (max-width: 650px) {
+    flex-direction: ${({ mobileDir }) => (mobileDir ? mobileDir : "row")};
+  }
+`;
+
+export const ColoredBox = styled.div`
+  background: var(--secondary);
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+`;
+
+export const SaleHeader = styled.p`
+  color: var(--primary);
+  font-weight: bold;
+  font-size: 1.5rem;
+`;
+
+export const CrossedOutText = styled.p`
+  text-decoration-line: line-through;
+  color: var(--gray-blue);
+  font-weight: bold;
+  font-size: 1.2rem;
+  margin-top: 0.75rem;
+  margin-bottom: 2rem;
+`;
+
+export const AddInput = styled.div`
+  background: var(--light-gray-blue);
+  width: 40%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.75rem;
+  border-radius: 1rem;
+
+  @media (max-width: 650px) {
+    width: 100%;
+  }
+`;
+
+export const InputButton = styled.p`
+  font-weight: bold;
+  color: var(--primary);
+  font-size: 2rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.5);
+  }
+`;
+
+export const Button = styled.button`
+  background: var(--primary);
+  border: none;
+  padding: 1.25rem;
+  width: 60%;
+  border-radius: 1rem;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  grid-gap: 1rem;
+  font-weight: bold;
+  font-size: 1.15rem;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  @media (max-width: 650px) {
+    width: 100%;
   }
 `;
